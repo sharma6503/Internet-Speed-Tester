31409d7242b4294d124d08e4080ff5ef2eab6728
sharma6503 <128107952+sharma6503@users.noreply.github.com>
2024-06-03 16:54:37 +0530

    Flet_9-Internet Speed Test App.py

--- Flet_9-Internet Speed Test App.py
+++ Flet_9-Internet Speed Test App.py
@@ -2,7 +2,7 @@ import flet as ft
 
 import speedtest
 from time import sleep
-
+from  typewriter_control import Typewriter
 
 def main(page: ft.Page):
     page.title = 'Internet Speed Test'
@@ -30,20 +30,20 @@ def main(page: ft.Page):
                         ft.Text(value='Speed', font_family='font3', size=50, color='#115852')]
                        , alignment='center')
     # Container lines
-    line1 = ft.Text(value='>Press Start.....', font_family='font4')
-    line2 = ft.Text(value='', font_family='font4')
-    line3 = ft.Text(value='', font_family='font4')
+    line1 = Typewriter(value='>Press Start.....', font_family='font4')
+    line2 = Typewriter(value='', font_family='font4')
+    line3 = Typewriter(value='', font_family='font4')
     progess1 = ft.ProgressBar(width=400, color='#115852', opacity=0)
-    progess_text1 = ft.Text('  ', font_family='font4', opacity=0)
+    progess_text1 =Typewriter('  ', font_family='font4', transperancy=False)
     progess_row1 = ft.Row([progess_text1, progess1])
-    line4 = ft.Text(value='', font_family='font4')
-    line5 = ft.Text(value='', font_family='font4')
-    line6 = ft.Text(value='', font_family='font4')
+    line4 = Typewriter(value='', font_family='font4')
+    line5 = Typewriter(value='', font_family='font4')
+    line6 = Typewriter(value='', font_family='font4')
     progess2 = ft.ProgressBar(width=400, color='#115852', opacity=0)
-    progess_text2 = ft.Text('  ', font_family='font4', opacity=0)
+    progess_text2 = Typewriter('  ', font_family='font4',transperancy=False)
     progess_row2 = ft.Row([progess_text2, progess2])
-    line7 = ft.Text(value='', font_family='font4')
-    line8 = ft.Text(value='', font_family='font4')
+    line7 = Typewriter(value='', font_family='font4')
+    line8 = Typewriter(value='', font_family='font4')
 
     lines = ft.Column([line1, line2, line3, progess_row1, line4, line5, line6, progess_row2, line7, line8])
 
@@ -61,31 +61,42 @@ def main(page: ft.Page):
         progess_row2.opacity = 0
         progess2.opacity = 0
         progess2.value = None
-        line1.value = ''
-        line2.value = ''
-        line3 .value= ''
-        line4 .value= ''
-        line5 .value= ''
-        line6 .value= ''
-        line7 .value= ''
-        line8 .value= ''
+        line1.text = ''
+        line1.update()
+        line2.text= ''
+        line2.update()
+        line3 .text= ''
+        line3.update()
+        line4 .text= ''
+        line4.update()
+        line5 .text= ''
+        line5.update()
+        line6 .text= ''
+        line6.update()
+        line7 .text= ''
+        line7.update()
+        line8 .text= ''
+        line8.update()
         speed_container.update()
 
         speed_container.width = 750
         speed_container.height = 400
-        line1.value = '>> Calculating download speed,Please Wait...'
+        line1.text = '>> Calculating download speed,Please Wait...'
         speed_container.update()
         sleep(1)
+        line1.update()  
 
         ideal_server = st.get_best_server()  # to connect to the best possible server in our region
         city = ideal_server['name']
         country = ideal_server['country']
         country_code = ideal_server['cc']
-        line2.value = f'>> Finding the best possible servers in {city},{country},({country_code})'
+        line2.text = f'>> Finding the best possible servers in {city},{country},({country_code})'
+        line2.update()
         speed_container.update()
         sleep(1)
 
-        line3.value = '>> Connection established ,status OK,fetching download speed'
+        line3.text = '>> Connection established ,status OK,fetching download speed'
+        line3.update()
         speed_container.update()
         sleep(1)
 
@@ -97,15 +108,18 @@ def main(page: ft.Page):
         download_speed = st.download() / 1024 / 1024  # bytes per sec to bits per sec
         progess1.value = 1  # completed progress
         line4.color = '#115852'
-        line4.value = f'>> The download speed is {str(round(download_speed, 2))} Mbps'
+        line4.text = f'>> The download speed is {str(round(download_speed, 2))} Mbps'
+        line4.update()
         speed_container.update()
         sleep(1)
 
-        line5.value = '>> Calculating upload speed,please wait...'
+        line5.text = '>> Calculating upload speed,please wait...'
+        line5.update()
         speed_container.update()
         sleep(1)
 
-        line6.value = '>> Executing upload script,hold on'
+        line6.text = '>> Executing upload script,hold on'
+        line6.update()
         speed_container.update()
         sleep(1)
 
@@ -117,11 +131,13 @@ def main(page: ft.Page):
         upload_speed = st.upload() / 1024 / 1024  # bytes per sec to bits per sec
         progess2.value = 1
         line7.color = '#115852'
-        line7.value = f'>> The upload speed is {str(round(upload_speed, 2))} Mbps'
+        line7.text = f'>> The upload speed is {str(round(upload_speed, 2))} Mbps'
+        line7.update()
         speed_container.update()
         sleep(1)
 
-        line8.value = '>> Task completed successfully\n\n>> App Developer : Sharma G ,Linkedin Profile:https://www.linkedin.com/in/sharma-g-124923253'
+        line8.text = '>> Task completed successfully\n\n>> App Developer : Sharma G ,Linkedin Profile:https://www.linkedin.com/in/sharma-g-124923253'
+        line8.update()
         speed_container.update()
 
     # Components
